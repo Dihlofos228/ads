4d7964574183e0dd91ab76bf0644a45ee987c264
Nikolay1930 <61801587+Nikolay1930@users.noreply.github.com>
2023-07-13 16:12:45 +0300

    Add files via upload

---
+++ advertisements/test_388/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import func_388
+
+urlpatterns = [
+    path('', func_388)
+]
---
+++ advertisements/test_388/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def func_388(request):
+    return HttpResponse('388')
+
